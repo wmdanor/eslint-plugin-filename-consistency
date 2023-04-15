94d23b74ce16e69641abd89ca52fada2ee873745
wmdanor <wmdanor@gmail.com>
2023-04-15 18:33:14 +0100

    Fix PascalCase regular expression

--- src/rules/match/constants.ts
+++ src/rules/match/constants.ts
@@ -2,7 +2,7 @@ import { MatchOptionsCase } from './types';
 
 export const cases: Readonly<Record<MatchOptionsCase, RegExp>> = {
   camel: /^([a-z][a-z0-9]*)(([A-Z]|[.][a-z])[a-z0-9]*)*$/,
-  pascal: /^([A-Z][a-z0-9]*)(([A-Z]|[.][A-Z])[a-z0-9]*)*$/,
+  pascal: /^([A-Z][a-z0-9]*)(([A-Z]|[.][a-zA-Z])[a-z0-9]*)*$/,
   snake: /^([a-z][a-z0-9]*)([_.][a-z0-9]+)*$/,
   kebab: /^([a-z][a-z0-9]*)([-.][a-z0-9]+)*$/,
 };
